02b735a3049eb5ac6e73049e01e7a274ef82b7b1
Benoît Eveillard <32536347+benoiteveillard@users.noreply.github.com>
2022-12-12 17:52:01 +0100

    added a comment

--- home.js
+++ home.js
@@ -1,3 +1,4 @@
+  // redirect to /powered-by if the utm_source equals widget
   if(location.search == "?utm_source=widget" && location.pathname=="/") {
     window.location.replace("https://www.gorgias.com/powered-by");
   }
