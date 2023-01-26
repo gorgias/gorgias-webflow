bb81dfefcce0fcf2589fa49fc42859f220c889ae
Benoît Eveillard <b.eveillard9@gmail.com>
2023-01-26 16:36:31 -0500

    "folders OK"

--- home.js
+++
@@ -1,5 +0,0 @@
-  // redirect to /powered-by if the utm_source equals widget
-  if(location.search == "?utm_source=widget" && location.pathname=="/") {
-    window.location.replace("https://www.gorgias.com/powered-by");
-    console.log('julien');
-  }
