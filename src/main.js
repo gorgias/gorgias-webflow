1910024d4979c8bdcf11ffa46153c35a7dd2256f
Julien Marcialis <julien.marcialis@gmail.com>
2023-01-27 11:20:57 +0100

    Organize the folder and use main.js (#12)

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import estimatePrice from './js/pricing'
+
+var Webflow = Webflow || [];
+Webflow.push(function () {
+
+    var path = location.path;
+    
+    // pricing page
+    if(path == '/pricing'){
+        
+        $(document).ready(estimatePrice);
+        $("form[name='wf-form-pricing-form'").change(estimatePrice);
+        $(".tabs-plan__pricing >a").click(estimatePrice);
+        $(".tabs-menu__pricing.w-tab-menu>a").click(estimatePrice);
+
+        $(".wrapper-flex-right__tab-pane-pricing .link").click(function(){
+            var planPeriod = $('.tabs-menu__pricing.w-tab-menu>a.w--current')[0].textContent.toLowerCase();
+            var planName = $('.tabs-plan__pricing .w-tab-pane.w--tab-active .tabs-plan__pricing .w--current .heading-tab-pane__pricing')[0].textContent.toLowerCase();
+            window.location.href = 'https://www.gorgias.com/demo?plan_name='+ planName +'&period=' + planPeriod;
+        });
+    }
+
+})
