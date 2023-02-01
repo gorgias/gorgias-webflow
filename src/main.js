10d0408f24d29891ff2688efd73679e0c4bdeb63
Julien Marcialis <julien.marcialis@gmail.com>
2023-02-01 22:26:26 +0100

    add css for pricing page

--- src/main.js
+++ src/main.js
@@ -4,15 +4,28 @@ Webflow.push(function () {
     var url = window.location.href;
     var debug = url.includes('debug');
     var scriptBase = debug ? "http://127.0.0.1:5500" : "https://cdn.jsdelivr.net/gh/gorgias/gorgias-webflow@latest";
+    // Get HTML head element
 
-    (async () => {
+    var head = document.getElementsByTagName('HEAD')[0];
         
         // pricing page
         if(path == '/pricing' || path == '/github-test'){
-            import( scriptBase + '/src/js/pricing.js');
+            (async () => {
+                import( scriptBase + '/src/js/pricing.js');
+            })();
+            // Create new link Element
+            var link = document.createElement('link');
+            // set the attributes for link element
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            link.href = scriptBase + '/src/css/pricing.css';
+            // Append link element to HTML head
+            head.appendChild(link);
+
+            console.log('test css');
             import('https://cdn.jsdelivr.net/npm/@finsweet/attributes-mirrorclick@1/mirrorclick.js');
         }
         
-    })();
+
 
 })
