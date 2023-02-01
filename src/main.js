7b22a78cc8c1e774b432e29c4bc647048ce0c17e
Julien Marcialis <julien.marcialis@gmail.com>
2023-02-01 23:01:16 +0100

    Creation of all.js to be loaded on every pages

--- src/main.js
+++ src/main.js
@@ -1,31 +1,19 @@
+var path = window.location.pathname;
+var url = window.location.href;
+var debug = url.includes('debug');
+scriptBase = debug ? "http://127.0.0.1:5500" : "https://cdn.jsdelivr.net/gh/gorgias/gorgias-webflow@latest";
+var head = document.getElementsByTagName('HEAD')[0]; // Get HTML head element
+
 var Webflow = Webflow || [];
 Webflow.push(function () {
-    var path = window.location.pathname;
-    var url = window.location.href;
-    var debug = url.includes('debug');
-    var scriptBase = debug ? "http://127.0.0.1:5500" : "https://cdn.jsdelivr.net/gh/gorgias/gorgias-webflow@latest";
-    // Get HTML head element
 
-    var head = document.getElementsByTagName('HEAD')[0];
-        
+    // all pages
+    $.getScript( scriptBase + '/src/js/all.js', function(){
         // pricing page
         if(path == '/pricing' || path == '/github-test'){
-            (async () => {
-                import( scriptBase + '/src/js/pricing.js');
-            })();
-            // Create new link Element
-            var link = document.createElement('link');
-            // set the attributes for link element
-            link.rel = 'stylesheet';
-            link.type = 'text/css';
-            link.href = scriptBase + '/src/css/pricing.css';
-            // Append link element to HTML head
-            head.appendChild(link);
-
-            console.log('test css');
             import('https://cdn.jsdelivr.net/npm/@finsweet/attributes-mirrorclick@1/mirrorclick.js');
+            newCdnStyle('/src/css/pricing.css');
+            newCdnAsyncScript('/src/js/pricing.js');
         }
-        
-
-
+    });
 })
