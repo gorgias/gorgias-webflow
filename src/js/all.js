41255270e2c7eb2083df9e15881f460a172eb992
Julien Marcialis <julien.marcialis@gmail.com>
2023-02-10 16:40:54 +0100

    fix segment snippet

--- src/js/all.js
+++ src/js/all.js
@@ -64,35 +64,10 @@ function getCookie(cname) {
 // Segment script
 // use a different write key in webflow development 
 var SEGMENT_WRITE_KEY = window.location.hostname === 'www.gorgias.com' ? "4K8cOhGhUlCNfE3rlsYVGBTBAR92Q2Nk": "nPhO6SyfURLuit010EVhib2Xe8t9MgwS";
-!function(){
-    var analytics=window.analytics=window.analytics||[];
-    if(!analytics.initialize) if(analytics.invoked)
-        window.console && console.error && console.log("Segment snippet included twice.");
-    else{
-        analytics.invoked=!0;
-        analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];
-        analytics.factory=function(t){
-            return function(){
-                var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics
-            }
-        };
-        for(var t=0;t<analytics.methods.length;t++){
-            var e=analytics.methods[t];analytics[e]=analytics.factory(e)
-        }
-        analytics.load=function(t,e){
-            var n=document.createElement("script");
-            n.type="text/javascript";
-            n.async=!0;
-            n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";
-            var a=document.getElementsByTagName("script")[0];
-            a.parentNode.insertBefore(n,a);
-            analytics._loadOptions=e
-        };
-        analytics.SNIPPET_VERSION="4.1.0";
-        analytics.load(SEGMENT_WRITE_KEY);
-        analytics.page(window.location.pathname);
-    }
-}();
+!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t,e){var n=document.createElement("script");n.type="text/javascript";n.async=!0;n.src="https://cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(n,a);analytics._loadOptions=e};analytics.SNIPPET_VERSION="4.1.0";
+analytics.load(SEGMENT_WRITE_KEY);
+analytics.page(window.location.pathname);
+}}();
 
 // function to get value of an utm parameters
 function getParam(name) {
