ca35724b08c6fbb6839236ada6c4831e7bc72d2a
Julien Marcialis <julien.marcialis@gmail.com>
2023-03-23 01:01:48 +0100

    postlog script added (#49)

---
+++ src/js/posthog.js
@@ -0,0 +1,24 @@
+// var path = window.location.pathname;
+
+// if(path.includes('/l/')){
+
+// // Ensure flags are loaded before usage.
+// // You'll only need to call this on the code for when the first time a user visits.
+// posthog.onFeatureFlags(function() {
+//     if (posthog.isFeatureEnabled('all-visitors')) {
+//         window.posthog.startSessionRecording();
+//         console.log("onFeatureFlags > isFeatureEnabled");
+//     }
+// })
+
+// // Otherwise, you can just do
+
+// if (posthog.isFeatureEnabled('all-visitors')) {
+//     // do something
+//     console.log("isFeatureEnabled");
+
+// }
+
+// }
+
+
