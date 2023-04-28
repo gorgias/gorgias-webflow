6ed7bec804592747de347888937c16b467a4794f
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-04-28 08:12:32 -0700

    merge main (#74)
    * adding new path for pricing test (#70)
    
    * Update main.js (#71)
    
    * New/different logos for selected countries (#73)
    
    * show different logos based on country
    
    with A/B testing for posthog experiment
    
    * check path for prod and testing also
    
    * merge main  (#72)
    
    * adding new path for pricing test (#70)
    
    * Update main.js (#71)

--- src/js/posthog.js
+++ src/js/posthog.js
@@ -14,3 +14,45 @@ if(
     })
 }
 
+if (path === '/' || path === '/pages/home-draft') {
+  posthog.onFeatureFlags(() => {
+    posthog.feature_flags.override({'customer-logos': 'test'});
+    const logosToSelect = document.getElementsByClassName("customer_logo-collection");
+    
+    if (posthog.getFeatureFlag('customer-logos') === 'test') {
+      $.ajax({
+        url: 'https://cloudflare.com/cdn-cgi/trace',
+        success: function(data) {
+          const country = data.match(/loc=(.+)/)[1];
+          const countryToWebflowIdentifier = {
+            "AU": 'australia',
+            "CA": 'canada',
+            "FR": 'france',
+            "UK": 'united-kingdom',
+            "US": 'united-states'
+          };
+
+          if (countryToWebflowIdentifier.hasOwnProperty(country)) {
+            logosToSelect[0].style.display = "none";
+            const showLogosByCountry = Array.from(logosToSelect)
+              .find(el => el.classList.contains(countryToWebflowIdentifier[country]));
+            if (showLogosByCountry) {
+              showLogosByCountry.style.display = 'block';
+            }
+          } else {
+             logosToSelect[0].style.display = 'block';
+          }
+        }
+      })
+    } else {
+      logosToSelect[0].style.display = 'block';
+      // It's a good idea to let control variant always be the default behaviour,
+      // so if something goes wrong with flag evaluation, you don't break your app.
+    }
+
+    // Clear the overrides for all flags
+    // posthog.feature_flags.clearOverrides();
+  })
+} 
+
+
