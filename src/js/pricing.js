ba801a6fa1e7b763c15dd8cbbdfba65e2f8c6485
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-05-09 21:01:43 +0300

    add more changes

--- src/js/pricing.js
+++ src/js/pricing.js
@@ -113,72 +113,23 @@ const smsDropdownPrice = {
 }
 let selectedPlan = templatePagesPaths ? pricingPlansTemplates[1] : pricingPlans[2]
 let selectedPeriod = 'monthly'
-const setPlanPrices = (getEl, pricingTabs, planPeriods) => {
+const setPlanPrices = (getEl) => {
   const priceTabs = getEl('heading-tab-pane__pricing price')
-  priceTabs[0].innerHTML = planPricingInit[0]
-  priceTabs[1].innerHTML = planPricingInit[1]
-  priceTabs[2].innerHTML = planPricingInit[2]
-  priceTabs[3].innerHTML = planPricingInit[3]
-  priceTabs[5].innerHTML = planPricingInit[6]
-  priceTabs[6].innerHTML = planPricingInit[7]
-  priceTabs[7].innerHTML = planPricingInit[8]
-  getEl('text-link__pricing')[0].innerHTML = planPricingInit[4]
-  getEl('text-link__pricing')[1].innerHTML = planPricingInit[4]
-  planPeriods[0].click()
-  pricingTabs[2].click()
-}
-
-// price calculation begins here
-const estimatePrice = (getEl, getElId) => {
-  const planPeriods = getEl('text-menu__pricing') 
-  const isAutomationToggleActive = getEl('w-checkbox-input')[0].classList.contains('w--redirected-checked')
-  const isVoiceToggleActive = getEl('w-checkbox-input')[1].classList.contains('w--redirected-checked')
-  const isSmsToggleActive = getEl('w-checkbox-input')[2].classList.contains('w--redirected-checked')
-  const automationPriceEl = getElId('pricing-automation')
-  const voicePriceEl = getElId('pricing-phone')
-  const smsPriceEl = getElId('pricing-sms')
-  // const showHideDropdown = (el, show) => show ? el.classList.removeClass('hidden') : el.classList.addClass('hidden') 
-  let ctaTextDisplay = document.querySelectorAll(".button.pricing")[0].innerHTML;
-  let ctaHrefDisplay = document.querySelectorAll(".button.pricing")[0].href;
-  const isEnterprisePlan = selectedPlan.name === 'enterprise'
-  const automationDropdown = getElId('automation-interaction-2')
-  const voiceDropdown = getElId('number-phone-interaction-2')
-  const smsDropdown = getElId('number-sms-interaction-2')
-
-  if (selectedPlan.name =='starter') {
-    Array.from(getEl('wrapper-master-select__pricing')).forEach(el => el.style['pointer-events'] = 'none')
-      automationPriceEl.style.color = "#afafaf";
-      planPeriods[1].style['pointer-events'] = 'none';
-      planPeriods[1].style.opacity = '0.4'; 
-   } else {
-    $("#wf-form-pricing-form .wrapper-master-checkbox__pricing>*:nth-child(1n) .heading-text-content__pricing span").css("color", "#1a9970");
-    Array.from(getEl('wrapper-master-select__pricing')).forEach(el => el.style['pointer-events'] = 'auto')
+  priceTabs[0].innerHTML = templatePagesPaths ? planPricingInitTemplate[0] : planPricingInit[0]
+  priceTabs[1].innerHTML = templatePagesPaths ? planPricingInitTemplate[1] : planPricingInit[1]
+  priceTabs[2].innerHTML = templatePagesPaths ? planPricingInitTemplate[2] : planPricingInit[2]
+  priceTabs[5].innerHTML = templatePagesPaths ? planPricingInitTemplate[5] : planPricingInit[6] 
+  priceTabs[6].innerHTML = templatePagesPaths ? planPricingInitTemplate[6] : planPricingInit[7] 
+  
+  if (!templatePagesPaths) {
+    priceTabs[3].innerHTML = planPricingInit[3] 
+  } else {
+    priceTabs[4].innerHTML = planPricingInitTemplate[4] 
+    priceTabs[7].innerHTML = planPricingInit[8]
   }
-
-  const automationTotalPrice = isAutomationToggleActive 
-    ? automationDropdownPrice[automationDropdown.value[selectedPeriod]] 
-    : automationPrice[selectedPlan.name][selectedPeriod] 
-  const voiceTotalPrice = isVoiceToggleActive 
-    ? voiceDropdownPrice[voiceDropdown.value[selectedPeriod]] 
-    : voicePrice[selectedPlan.name][selectedPeriod] 
-  const smsTotalPrice = isSmsToggleActive 
-    ? smsDropdownPrice[smsDropdown.value[selectedPeriod]] 
-    : smsPrice[selectedPlan.name][selectedPeriod] 
-  const checkIfString = (el) => typeof el === 'string'
-  const returnValue = (el) =>  checkIfString(el) ? el : '+$' + el + '/mo';
-  console.log(returnValue(voiceTotalPrice))
-  let totalPrice = selectedPlan.price + returnValue(automationTotalPrice) + returnValue(voiceTotalPrice) + returnValue(smsTotalPrice)
-  totalPrice = isEnterprisePlan && 'Custom Price'
-  ctaTextDisplay = isEnterprisePlan ? 'Contact us' : 'Start a free trial'
-  ctaHrefDisplay =  isEnterprisePlan 
-    ? 'https://www.gorgias.com/demo?plan_name='+ selectedPlan.name +'&period=' + selectedPeriod
-    : 'https://www.gorgias.com/signup?plan_name='+ selectedPlan.name +'&period=' + selectedPeriod
-
-    automationPriceEl.innerHTML = returnValue(automationTotalPrice);
-    voicePriceEl.innerHTML = returnValue(voiceTotalPrice)
-    smsPriceEl.innerHTML = returnValue(smsTotalPrice)
-    getEl('heading-tab-pane__pricing price-form')[0].innerHTML = totalPrice
 }
+const showHideDropdown = (el, show) => show ? el.classList.removeClass('hidden') : el.classList.addClass('hidden') 
+
 document.addEventListener("DOMContentLoaded", () => {
   const getEl = (val) => document.getElementsByClassName(val)
   const getElId = (val) => document.getElementById(val)
@@ -186,7 +137,60 @@ document.addEventListener("DOMContentLoaded", () => {
   const pricingTabs = getEl('tab-pane__pricing')
   const selects = document.getElementsByTagName('select');
   // set plan prices
-  setPlanPrices(getEl, pricingTabs, planPeriods)
+  setPlanPrices(getEl, pricingTabs)
+
+  // price calculation begins here
+  const estimatePrice = () => {
+    const isAutomationToggleActive = getEl('w-checkbox-input')[0].classList.contains('w--redirected-checked')
+    const isVoiceToggleActive = getEl('w-checkbox-input')[1].classList.contains('w--redirected-checked')
+    const isSmsToggleActive = getEl('w-checkbox-input')[2].classList.contains('w--redirected-checked')
+    const automationPriceEl = getElId('pricing-automation')
+    const voicePriceEl = getElId('pricing-phone')
+    const smsPriceEl = getElId('pricing-sms')
+    let ctaTextDisplay = document.querySelectorAll(".button.pricing")[0].innerHTML;
+    let ctaHrefDisplay = document.querySelectorAll(".button.pricing")[0].href;
+    const isEnterprisePlan = selectedPlan.name === 'enterprise'
+    const automationDropdown = getElId('Number-Automation-Addon-Interaction')
+    const voiceDropdown = getElId('number-phone-interaction-2')
+    const smsDropdown = getElId('number-sms-interaction-2')
+    showHideDropdown(automationDropdown, isAutomationToggleActive)
+    showHideDropdown(voiceDropdown, isVoiceToggleActive) 
+    showHideDropdown(smsDropdown, isSmsToggleActive)
+
+    if (selectedPlan.name =='starter') {
+      Array.from(getEl('wrapper-master-select__pricing')).forEach(el => el.style['pointer-events'] = 'none')
+        automationPriceEl.style.color = "#afafaf";
+        planPeriods[1].style['pointer-events'] = 'none';
+        planPeriods[1].style.opacity = '0.4'; 
+    } else {
+      $("#wf-form-pricing-form .wrapper-master-checkbox__pricing>*:nth-child(1n) .heading-text-content__pricing span").css("color", "#1a9970");
+      Array.from(getEl('wrapper-master-select__pricing')).forEach(el => el.style['pointer-events'] = 'auto')
+    }
+
+    const automationTotalPrice = isAutomationToggleActive 
+      ? automationDropdownPrice[automationDropdown.value[selectedPeriod]] 
+      : automationPrice[selectedPlan.name][selectedPeriod] 
+    const voiceTotalPrice = isVoiceToggleActive 
+      ? voiceDropdownPrice[voiceDropdown.value[selectedPeriod]] 
+      : voicePrice[selectedPlan.name][selectedPeriod] 
+    const smsTotalPrice = isSmsToggleActive 
+      ? smsDropdownPrice[smsDropdown.value[selectedPeriod]] 
+      : smsPrice[selectedPlan.name][selectedPeriod] 
+    const checkIfString = (el) => typeof el === 'string'
+    const returnValue = (el) =>  checkIfString(el) ? el : '+$' + el + '/mo';
+    console.log(returnValue(voiceTotalPrice))
+    let totalPrice = selectedPlan.price + returnValue(automationTotalPrice) + returnValue(voiceTotalPrice) + returnValue(smsTotalPrice)
+    totalPrice = isEnterprisePlan && 'Custom Price'
+    ctaTextDisplay = isEnterprisePlan ? 'Contact us' : 'Start a free trial'
+    ctaHrefDisplay =  isEnterprisePlan 
+      ? 'https://www.gorgias.com/demo?plan_name='+ selectedPlan.name +'&period=' + selectedPeriod
+      : 'https://www.gorgias.com/signup?plan_name='+ selectedPlan.name +'&period=' + selectedPeriod
+
+      automationPriceEl.innerHTML = returnValue(automationTotalPrice);
+      voicePriceEl.innerHTML = returnValue(voiceTotalPrice)
+      smsPriceEl.innerHTML = returnValue(smsTotalPrice)
+      getEl('heading-tab-pane__pricing price-form')[0].innerHTML = totalPrice
+  }
 
   // add event listener for when we change period tab from monthly to yearly and viceversa
   for (let i = 0; i < planPeriods.length; i++) {
@@ -200,22 +204,20 @@ document.addEventListener("DOMContentLoaded", () => {
   // add event listener for when we change dropdowns values
   for (let i = 0; i < selects.length; i++) {
     selects[i].addEventListener('change', function () {
-      estimatePrice(getEl, getElId)
+      estimatePrice()
     });
   }
   // find if we have selected checkboxes
   const checkBoxes = getEl('w--redirected-checked')
   for (let i = 0; i < pricingTabs.length; i++) {
     pricingTabs[i].addEventListener('click', function () {
-      if (i === 0) {
-        planPeriods[1].style['pointer-events'] = 'none';
-        planPeriods[1].style.opacity = '0.4'; 
-      } else {
-        planPeriods[1].style['pointer-events'] = 'auto';
-        planPeriods[1].style.opacity = '1';
+      if (!templatePagesPaths && i === 5) {
+        timeTab[0].click()
+        pricingTabs[0].click()
+        document.getElementsByClassName("tab-pane__pricing")[0].click()
       }
       selectedPlan = pricingPlans[i]
-      estimatePrice(getEl, getElId)
+      estimatePrice()
       if (checkBoxes.length) {
         // when changing plan we deselect them
         Array.from(checkBoxes).forEach(el => {
@@ -224,4 +226,9 @@ document.addEventListener("DOMContentLoaded", () => {
       }
     });
   }
+  planPeriods[1].addEventListener('click', function() {
+    if (pricingTabs[0].classList.contains('w--current')) {
+      pricingTabs[6].click()
+    }
+  })
 });
