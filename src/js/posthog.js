a8250c41ca802a168808db6e36d0969df2197c0a
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-06-18 20:51:50 +0300

    allow feature flag

--- src/js/posthog.js
+++ src/js/posthog.js
@@ -36,7 +36,7 @@ if (path === '/pages/home-draft' || path === '/demo' || path === '/demo-test') {
   var logosToSelect = document.getElementsByClassName("customer_logos-collection-wrapper");
   if(logosToSelect.length > 0){
     posthog.onFeatureFlags(() => {
-      // posthog.feature_flags.override({'customer-logos': 'variant'}); // to comment after testing
+      posthog.feature_flags.override({'customer-logos': 'variant'}); // to comment after testing
       if (posthog.getFeatureFlag('customer-logos') === 'variant') {
           const loc_code = sessionStorage.getItem("loc_code");
   
