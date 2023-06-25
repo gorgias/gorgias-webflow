f4a44274d29451b041634111b573763c38dc35db
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-06-25 19:02:09 +0300

    remove some code for error purpose

--- src/js/posthog.js
+++ src/js/posthog.js
@@ -1,26 +1,3 @@
-// var path = window.location.pathname;
-
-// In case of ad blocker, posthog experiments won't load.
-// You can use /experiments.js file to create a default path
-// Record session for the following pages
-if (
-  path.includes('/l/') || path.includes('/lp/') || path.includes('/lc/') // ads landing pages
-  ||
-  path.includes('/demo') // demo funnel
-  ||
-  path.includes('/signup') // signup funnels
-  ||
-  path.includes('/pages/') // landing pages
-  ||
-  path.includes('/comparison') // MOFU pages
-) {
-  posthog.onFeatureFlags(function() {
-    if (posthog.isFeatureEnabled('all-visitors')) {
-      window.posthog.startSessionRecording();
-    }
-  })
-}
-
 function showLogos(logosToSelect) {
   const loc_code = sessionStorage.getItem("loc_code")
 
