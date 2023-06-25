18e020727245ae0e4a27aa0cd605a91b662826c1
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-06-25 19:28:44 +0300

    readd deleted code

--- src/js/posthog.js
+++ src/js/posthog.js
@@ -1,3 +1,26 @@
+// var path = window.location.pathname;
+
+// In case of ad blocker, posthog experiments won't load.
+// You can use /experiments.js file to create a default path
+// Record session for the following pages
+if (
+  path.includes('/l/') || path.includes('/lp/') || path.includes('/lc/') // ads landing pages
+  ||
+  path.includes('/demo') // demo funnel
+  ||
+  path.includes('/signup') // signup funnels
+  ||
+  path.includes('/pages/') // landing pages
+  ||
+  path.includes('/comparison') // MOFU pages
+) {
+  posthog.onFeatureFlags(function() {
+    if (posthog.isFeatureEnabled('all-visitors')) {
+      window.posthog.startSessionRecording();
+    }
+  })
+}
+
 function showLogos(logosToSelect) {
   const loc_code = sessionStorage.getItem("loc_code")
 
@@ -42,9 +65,9 @@ function handleFeatureFlagsCommon() {
   const logosToSelect = document.getElementsByClassName("customer_logos-collection-wrapper")
   if (logosToSelect.length > 0) {
     posthog.onFeatureFlags(() => {
-      posthog.feature_flags.override({
-        'customer-logos': 'variant'
-      }); // to comment after testing
+      // posthog.feature_flags.override({
+        // 'customer-logos': 'variant'
+      // }); // to comment after testing
       if (posthog.getFeatureFlag('customer-logos') === 'variant') {
         showLogos(logosToSelect)
       } else {
