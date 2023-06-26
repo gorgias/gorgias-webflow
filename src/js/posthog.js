36b119199212a511051f6670785acb373dbd88e3
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-06-26 10:34:21 +0300

    add check for regular paths

--- src/js/posthog.js
+++ src/js/posthog.js
@@ -2,25 +2,6 @@
 
 // In case of ad blocker, posthog experiments won't load.
 // You can use /experiments.js file to create a default path
-// Record session for the following pages
-if (
-  path.includes('/l/') || path.includes('/lp/') || path.includes('/lc/') // ads landing pages
-  ||
-  path.includes('/demo') // demo funnel
-  ||
-  path.includes('/signup') // signup funnels
-  ||
-  path.includes('/pages/') // landing pages
-  ||
-  path.includes('/comparison') // MOFU pages
-) {
-  posthog.onFeatureFlags(function() {
-    if (posthog.isFeatureEnabled('all-visitors')) {
-      window.posthog.startSessionRecording();
-    }
-  })
-}
-
 function showLogos(logosToSelect) {
   const loc_code = sessionStorage.getItem("loc_code")
 
@@ -66,7 +47,7 @@ function handleFeatureFlagsCommon() {
   if (logosToSelect.length > 0) {
     posthog.onFeatureFlags(() => {
       // posthog.feature_flags.override({
-        // 'customer-logos': 'variant'
+      // 'customer-logos': 'variant'
       // }); // to comment after testing
       if (posthog.getFeatureFlag('customer-logos') === 'variant') {
         showLogos(logosToSelect)
@@ -83,4 +64,22 @@ function handleFeatureFlagsCommon() {
 if (posthog.__loaded) {
   if (path === '/demo-test') handleFeatureFlagsDemoTest()
   if (path === '/pages/home-draft' || path === '/demo') handleFeatureFlagsCommon()
+  // Record session for the following pages
+  if (
+    path.includes('/l/') || path.includes('/lp/') || path.includes('/lc/') // ads landing pages
+    ||
+    path.includes('/demo') // demo funnel
+    ||
+    path.includes('/signup') // signup funnels
+    ||
+    path.includes('/pages/') // landing pages
+    ||
+    path.includes('/comparison') // MOFU pages
+  ) {
+    posthog.onFeatureFlags(function() {
+      if (posthog.isFeatureEnabled('all-visitors')) {
+        window.posthog.startSessionRecording();
+      }
+    })
+  }
 }
