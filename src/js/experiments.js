64312c7112ee5560e215ecdf9c3a44c8a0848a59
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-07-13 19:21:46 +0300

    check for display none

--- src/js/experiments.js
+++ src/js/experiments.js
@@ -1,5 +1,11 @@
 function displayDefaultCustomerLogosList(logosToSelect) {
   setTimeout(function () {
+      // in case posthog does not load we still need to show main wrapper on demo page
+    const mainWrapperDemo = document.getElementsByClassName('demo_main-wrapper')[0]
+    const displayValue = window.getComputedStyle(mainWrapperDemo).getPropertyValue('display')
+    if (mainWrapperDemo && displayValue === 'none') {
+      mainWrapperDemo.style.display = 'block'
+    }
     // Check if none of the elements are displayed
     var noneDisplayed = true;
     for (var i = 0; i < logosToSelect.length; i++) {
@@ -17,9 +23,6 @@ function displayDefaultCustomerLogosList(logosToSelect) {
 }
 
 if (path === "/pages/home-draft" || path === "/demo" || path === "/demo-2") {
-  // in case posthog does not load we still need to show main wrapper on demo page
-  const mainWrapperDemo = document.getElementsByClassName('demo_main-wrapper')[0]
-  mainWrapperDemo && (mainWrapperDemo.style.display = 'block')
   
   var logosToSelect = document.getElementsByClassName(
     "customer_logos-collection-wrapper"
