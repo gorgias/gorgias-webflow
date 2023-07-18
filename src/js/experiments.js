1fac713093f85d2cb954d9079cfb5b108c07b5d2
IftodiRadu <130574320+IftodiRadu@users.noreply.github.com>
2023-07-18 15:46:46 +0300

    update for /demo-test-wider path

--- src/js/experiments.js
+++ src/js/experiments.js
@@ -1,11 +1,5 @@
 function displayDefaultCustomerLogosList(logosToSelect) {
-  setTimeout(function () {
-      // in case posthog does not load we still need to show main wrapper on demo page
-    const mainWrapperDemo = document.getElementsByClassName('demo_main-wrapper')[0]
-    const displayValue = window.getComputedStyle(mainWrapperDemo).getPropertyValue('display')
-    if (mainWrapperDemo && displayValue === 'none') {
-      mainWrapperDemo.style.display = 'block'
-    }
+  setTimeout(function() {
     // Check if none of the elements are displayed
     var noneDisplayed = true;
     for (var i = 0; i < logosToSelect.length; i++) {
@@ -22,12 +16,23 @@ function displayDefaultCustomerLogosList(logosToSelect) {
   }, 3000); // 3 seconds
 }
 
-if (path === "/pages/home-draft" || path === "/demo" || path === "/demo-2") {
-  
+if (path === "/pages/home-draft" || path === "/demo") {
   var logosToSelect = document.getElementsByClassName(
     "customer_logos-collection-wrapper"
   );
   if (logosToSelect.length > 0) {
     displayDefaultCustomerLogosList(logosToSelect);
+  }
+}
+
+if (path === "/demo-test-wider") {
+  // first we show old layout and then select logos
+  const oldLayoutDemo = document.getElementsByClassName('page_demo-old-layout')[0]
+  oldLayoutDemo.style.display = 'block'
+  const logosToSelect = document.getElementsByClassName(
+    "customer_logos-collection-wrapper"
+  )
+  if (logosToSelect.length > 0) {
+    displayDefaultCustomerLogosList(logosToSelect)
   }
 }
