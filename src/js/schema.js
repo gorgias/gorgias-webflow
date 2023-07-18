7f405a086db34c4d2aeec568c9c6d3920581438d
Julien Marcialis <julien.marcialis@gmail.com>
2023-07-18 11:29:30 +0200

    set conditions for schemas' scripts

--- src/js/schema.js
+++ src/js/schema.js
@@ -1,5 +1,6 @@
 // Schema
 
+
 /* Schema.org Website */
 var script1 = document.createElement('script');
 var scriptId1 = 'schema-website';
@@ -9,20 +10,40 @@ script1.text = '{"@context": "https://schema.org/","@type": "WebSite","name": "G
 $('body')[0].appendChild(script1);  
 /* END Schema.org Website */
 
-/* Schema.org Organization */
-var script2 = document.createElement('script');
-var scriptId2 = 'schema-organization';
-script2.setAttribute("type","application/ld+json");
-script2.setAttribute("id",scriptId2);
-script2.text = '{"@context": "https://schema.org/","@type": "Organization","name": "Gorgias","url": "https://www.gorgias.com/","logo": "https://uploads-ssl.webflow.com/5e4ff204e7b6f80e402d407a/5e693ac1927efe8a51c9662b_logo-gorgias.svg"}';
-$('body')[0].appendChild(script2);  
-/* END Schema.org Organization */
 
-/* Schema.org Software Application */
-var script3 = document.createElement('script');
-var scriptId3 = 'schema-software-application';
-script3.setAttribute("type","application/ld+json");
-script3.setAttribute("id",scriptId3);
-script3.text = '{"@context": "https://schema.org/","@type": "SoftwareApplication","name": "Gorgias","aggregateRating": {"@type": "AggregateRating","ratingValue": "4.6","ratingCount": "484","reviewCount": "484","bestRating": "5","worstRating": "2"},"applicationCategory": "BusinessApplication","operatingSystem": ""}';
-$('body')[0].appendChild(script3);  
-/* END Schema.org Software Application */
+
+if(
+    path.includes('/pricing') ||
+    path.includes('/product') ||
+    path.includes('/pages') ||
+    path.includes('/magento') ||
+    path.includes('/bigcommerce') ||
+    path.includes('/shopify') ||
+    path.includes('/shopify-plus') ||
+    path.includes('/signup') ||
+    path.includes('/demo') ||
+    path === '/'
+) {
+    /* Schema.org Software Application */
+    var script3 = document.createElement('script');
+    var scriptId3 = 'schema-software-application';
+    script3.setAttribute("type","application/ld+json");
+    script3.setAttribute("id",scriptId3);
+    script3.text = '{"@context": "https://schema.org/","@type": "SoftwareApplication","name": "Gorgias","aggregateRating": {"@type": "AggregateRating","ratingValue": "4.6","ratingCount": "484","reviewCount": "484","bestRating": "5","worstRating": "2"},"applicationCategory": "BusinessApplication","operatingSystem": ""}';
+    $('body')[0].appendChild(script3);  
+    /* END Schema.org Software Application */
+}
+
+if(
+    path.includes('/about-us') ||
+    path === '/'
+) {
+    /* Schema.org Organization */
+    var script2 = document.createElement('script');
+    var scriptId2 = 'schema-organization';
+    script2.setAttribute("type","application/ld+json");
+    script2.setAttribute("id",scriptId2);
+    script2.text = '{"@context": "https://schema.org/","@type": "Organization","name": "Gorgias","url": "https://www.gorgias.com/","logo": "https://uploads-ssl.webflow.com/5e4ff204e7b6f80e402d407a/5e693ac1927efe8a51c9662b_logo-gorgias.svg"}';
+    $('body')[0].appendChild(script2);  
+    /* END Schema.org Organization */
+}
