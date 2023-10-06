1d7d899001b5ec406d367070f3eee84a3a4e8c11
Julien Marcialis <julien.marcialis@gmail.com>
2023-10-06 15:00:28 +0200

    implement xp "small demo  form" on /wip/demo

--- src/js/demo.js
+++ src/js/demo.js
@@ -2,7 +2,6 @@
 window.addEventListener("message", function(event) {
     // when form is ready
     if(event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormReady' && event.data.id === "ef92ccce-92bd-4010-847a-793f56b6b353") {
-  
         // if url contains reamaze --> autocomplete current_helpdesk field if it exist and then and hide it
         if($('div.hs_demo_current_helpdesk').length  && location.href.includes('reamaze') == true){
             $('select[name=demo_current_helpdesk]').val('Reamaze').change();
@@ -209,4 +208,31 @@ function customizedCustomerLogos () {
     }
   }
   
-  customizedCustomerLogos();
+customizedCustomerLogos();
+
+
+if (path === '/wip/demo') {
+    $('demo-form-hubspot-cp').hide();
+
+    window.addEventListener("message", function(event) {
+        if(event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormReady' && event.data.id === "340a1d09-61d3-43c5-be9b-20c3ec22f665") {
+            $('div[class*=demo-form-hubspot] .hs-number_of_agents, div[class*=demo-form-hubspot] .hs-phone, div[class*=demo-form-hubspot] .hs-demo_ecommerce_platform, div[class*=demo-form-hubspot] .hs-demo_product_interest').hide( function (){
+                $('demo-form-hubspot-cp').fadeIn(1000);
+            })
+        }
+
+        $('div[class*=demo-form-hubspot] .hs-company_domain input').on('input', function() {
+            var companyDomain = $(this).val();
+            var domainPattern = /^(?:https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(?:\/.*)?$/;
+
+            if (domainPattern.test(companyDomain)) {
+                setTimeout(function() {
+                    $('div[class*=demo-form-hubspot] .hs-number_of_agents, div[class*=demo-form-hubspot] .hs-phone, div[class*=demo-form-hubspot] .hs-demo_ecommerce_platform, div[class*=demo-form-hubspot] .hs-demo_product_interest').fadeIn(1000);
+                }, 250)
+            } 
+        });
+    })
+}
+    
+
+
