feca9bfa8c2014c9f2d499aeecd4f3bfcbc0e324
Julien Marcialis <julien.marcialis@gmail.com>
2023-10-16 14:16:57 +0200

    fix css submit button and rules display fields (#202)

--- src/js/demo-2.js
+++ src/js/demo-2.js
@@ -2,7 +2,7 @@ if (path === '/wip/demo' || path === '/demo-2') {
   $('.demo-form-hubspot-cp').hide();
   window.addEventListener("message", function(event) {
       if(event.data.type === 'hsFormCallback' && event.data.eventName === 'onFormReady' && event.data.id === "ef92ccce-92bd-4010-847a-793f56b6b353") {
-          if($('div[class*=demo-form-hubspot] .hs-company_domain input').val() != '' && $('div[class*=demo-form-hubspot] .hs-company_domain input').val().length > 2){
+          if($('div[class*=demo-form-hubspot] .hs-company_domain input').val() != ''){
               $('.demo-form-hubspot-cp').fadeIn(500);
           }
           else{
@@ -15,7 +15,8 @@ if (path === '/wip/demo' || path === '/demo-2') {
 
       $('div[class*=demo-form-hubspot] .hs-company_domain input').on('input', function() {
           var companyDomain = $(this).val();
-          if (companyDomain != '' && companyDomain.length > 2) {
+          var domainPattern = /^(?:https?:\/\/)?([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})(?:\/.*)?$/;
+          if (companyDomain != '' && domainPattern.test(companyDomain)) {
               setTimeout(function() {
                   $('div[class*=demo-form-hubspot] .hs-number_of_agents, div[class*=demo-form-hubspot] .hs-phone, div[class*=demo-form-hubspot] .hs-demo_ecommerce_platform, div[class*=demo-form-hubspot] .hs-demo_product_interest').fadeIn(1000);
               }, 250)
