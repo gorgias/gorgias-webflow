179d66699230dc6c6cfacc2ff3ec5d08f3f4be2e
Gorgias-JosephBongrand <joseph.bongrand@gorgias.com>
2024-04-10 10:20:18 +0200

    Update hidenavigations.js

--- src/js/hidenavigations.js
+++ src/js/hidenavigations.js
@@ -1,7 +1,9 @@
 const navbar = document.querySelector('.sticky-wrapper');
 const footer = document.querySelector('.new-footer');
 
+/*
 if (path.includes('/fr/home')) {
     navbar.classList.add('fr-hidden');
     footer.remove();
 } 
+*/
