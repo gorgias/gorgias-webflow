b67287f06bc4c3f6fb1108c1d90a8bfd7d91c44e
Gorgias-JosephBongrand <164010322+Gorgias-JosephBongrand@users.noreply.github.com>
2025-01-31 16:38:05 +0100

    Update main.js (#424)

--- src/main.js
+++ src/main.js
@@ -36,9 +36,9 @@ Webflow.push(function () {
     }
 
     // If page is not /cx-audit then load the following scripts
-    if (!path.includes('/cx-audit')) {
-      newScript(scriptBase + '/src/js/gorgiaschat'+minBase+'.js','body',1);
-    }
+    // if (!path.includes('/cx-audit')) {
+    //   newScript(scriptBase + '/src/js/gorgiaschat'+minBase+'.js','body',1);
+    // }
 
     if ( path.includes('/comparison/')){
       newScript(scriptBase + '/src/js/competitors'+minBase+'.js','body', 1);
