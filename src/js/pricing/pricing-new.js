2dc46690775b25320f2b156d581aced302d2ae8a
Gorgias-JosephBongrand <joseph.bongrand@gorgias.com>
2025-06-04 15:27:11 +0200

    Update pricing-new.js

--- src/js/pricing/pricing-new.js
+++ src/js/pricing/pricing-new.js
@@ -537,24 +537,6 @@ $('.pricing_toggle-trigger[tab-link]').on('click', function () {
   }
 });
 
-// Close dropdown when clicking a link inside it
-$('[g-dropdown-item="link"]').on('click', function (e) {
-
-  const $dropdown = $(this).closest('.w-dropdown');
-  const $toggle = $dropdown.find('.w-dropdown-toggle');
-  const $list = $dropdown.find('.w-dropdown-list');
-
-  // Step 1: Remove open state manually
-  $toggle.removeClass('w--open').attr('aria-expanded', 'false');
-  $list.removeClass('w--open');
-
-  // Step 2: Trigger mouseup to sync Webflow's internal state
-  // Webflow uses mouseup on body to reset dropdown state
-  $(document).trigger('mouseup');
-  $(document).trigger('touchend');
-
-  console.log('Dropdown closed and Webflow state reset');
-});
 
   /**
    * Initialize component: set default UI and bind events.
