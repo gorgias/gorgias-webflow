9d9328eb30b4644d47868f0860846cad39035b60
Gorgias-JosephBongrand <164010322+Gorgias-JosephBongrand@users.noreply.github.com>
2025-07-16 18:26:19 +0200

    Update ai agent pages (#532)
    * Update, removed stacked animation
    
    * Update stacked
    
    * Update to only show on mobile
    
    * Update main
    
    * Update routing support skills
    
    * Update stack js so animation is faster
    
    * Hide markers
    
    * Added condition to not run under 991px
    
    * Fine tuning speed

--- src/js/stacked.js
+++ src/js/stacked.js
@@ -2,7 +2,7 @@ if (window.innerWidth > 991) {
   gsap.registerPlugin(ScrollTrigger);
 
   // --- EASY TWEAKS ---
-  const PER_LAYER_SCROLL = 400; // px of scroll per layer (adjust this for speed/feel)
+  const PER_LAYER_SCROLL = 500; // px of scroll per layer (adjust this for speed/feel)
   const LAYER_ANIMATION_DURATION = 2; // seconds for each y movement
   const layerHeight = 32; // px vertical offset per layer
 
@@ -15,7 +15,7 @@ if (window.innerWidth > 991) {
     scrollTrigger: {
       trigger: ".is-stacked-section",
       start: "top top",
-      end: "+=350vh",
+      end: "+=500vh",
       scrub: true,
       pin: true,
       //markers: true,
